d7548542441c324218b917339758a90aa722a9be
Praveenive <praveenhb0610@gmail.com>
2023-07-13 23:17:35 +0530

    updated

--- src/Base/Components/Faculty/UpdateFaculty.js
+++ src/Base/Components/Faculty/UpdateFaculty.js
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material'
 import { TextField } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 
 import * as yup from 'yup'
@@ -8,80 +8,121 @@ import { useFormik } from 'formik'
 import Base from '../../Base'
 
 const filledValidationSchema = yup.object({
-  teacherName:yup.string().required("Kindly update the name"),
-  subject:yup.string().required("Kindly update the subject").min("5","Fill valid batch"),
-  teachergender:yup.string().required("Kindly update gender"),
-  Role:yup.string().required("kindly update role")
+  teacherName: yup.string().required("Kindly update the name"),
+  subject: yup.string().required("Kindly update the subject").min(5, "Fill valid subject"),
+  teacherGender: yup.string().required("Kindly update gender"),
+  Role: yup.string().required("Kindly update role")
 })
-function UpdateFaculty({teacher,setTeacher,editid,setEditid}) {
-  const { handleSubmit,values,handleChange,errors } = useFormik({
-    initialValues:{
-      teacherName: "",subject:"",teachergender:"",Role:""
+
+function UpdateFaculty({ teacher, setTeacher, editid, setEditid }) {
+  const history = useHistory()
+  const editTeacher = teacher[editid]
+
+  const { handleSubmit, values, handleChange, errors } = useFormik({
+    initialValues: {
+      teacherName: editTeacher.teacherName || "",
+      subject: editTeacher.subject || "",
+      teacherGender: editTeacher.teacherGender || "",
+      Role: editTeacher.Role || ""
     },
-    validationSchema :filledValidationSchema,
-    onSubmit:(x)=>{
-      console.log("Onsubmit",x)
-      needupdate(x)
+    validationSchema: filledValidationSchema,
+    onSubmit: (x) => {
+      console.log("Onsubmit", x)
+      needUpdate(x)
     }
   })
-    const history =useHistory()
-    const editTeacher = teacher[editid]
-    const [teacherName,setTeachername]=useState("")
-    const [subject,setSubject]=useState("")
-    const [teacherGender,setTeacherGender]=useState("")
-    const [role,setRole]=useState("")
-    useEffect(()=>{
-        setTeachername(editTeacher.teacherName)
-        setSubject(editTeacher.subject)
-         setTeacherGender(editTeacher.teacherGender)
-         setRole(editTeacher.Role)
-    },[editTeacher])
-  
-    async function needupdate() {
-      const updateobj = {teacherName:values.teacherName,subject:values.subject,
-        teacherGender:values.teachergender,Role:values.Role }
-        const response =await fetch(`https://644f880bba9f39c6ab65caa9.mockapi.io/teacher/${editTeacher.id}`,{
-          method:"PUT",
-          body:JSON.stringify(updateobj),
-          headers:{
-            "Content-type":"application/json"
-          }
-        })
-        const data = await response.json()
-        if(data){
-    setTeacher((x) => {
-      const updated = [...x];
-      updated[editid] = updateobj;
-      return updated;})
+
+  useEffect(() => {
+    handleChange({ target: { name: 'teacherName', value: editTeacher.teacherName || "" } })
+    handleChange({ target: { name: 'subject', value: editTeacher.subject || "" } })
+    handleChange({ target: { name: 'teacherGender', value: editTeacher.teacherGender || "" } })
+    handleChange({ target: { name: 'Role', value: editTeacher.Role || "" } })
+  }, [editTeacher, handleChange])
+
+  async function needUpdate(updateData) {
+    const updateObj = {
+      teacherName: updateData.teacherName,
+      subject: updateData.subject,
+      teacherGender: updateData.teacherGender,
+      Role: updateData.Role
     }
-      history.push("/teachers")
-}
-    return (
-      <Base
-      title={"Welcome to Faculty Edit Section "}
-      about ={"To Fill these and Update Your data "    }
-      >
-        <form onSubmit={handleSubmit}>
-     <div className='input-list'>
-        <TextField id="filled-basic" label="Name" variant="filled" name='teacherName' type='teacherName'
-        onChange={handleChange} value={values.teacherName} />
-        <div style={{color:"crimson"}}>{errors ? errors.teacherName : ""}</div>
-        <TextField id="filled-basic" label="Subject" variant="filled" name='subject' type='subject'
-         onChange={handleChange} value={values.subject}/>
-         <div style={{color:"crimson"}}>{errors ? errors.subject : ""}</div>
-        <TextField id="filled-basic" label="Gender" variant="filled" name='teachergender' type='teachergender'
-        onChange={handleChange} value={values.teachergender}/>
-        <div style={{color:"crimson"}}>{errors ? errors.teachergender : ""}</div>
-        <TextField id="filled-basic" label="Role" variant="filled" name='Role' type='Role'
-        onChange={handleChange} value={values.Role}/>
-        <div style={{color:"crimson"}}>{errors ? errors.Role : ""}</div>
-        </div><br/>
-        <Button variant="contained" type='submit'>Update Students</Button>
-        </form>
-  
-  
-      </Base>
-    )
+
+    const response = await fetch(`https://644f880bba9f39c6ab65caa9.mockapi.io/teacher/${editTeacher.id}`, {
+      method: "PUT",
+      body: JSON.stringify(updateObj),
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+
+    const data = await response.json()
+    if (data) {
+      setTeacher((teachers) => {
+        const updatedTeachers = [...teachers];
+        updatedTeachers[editid] = updateObj;
+        return updatedTeachers;
+      });
+    }
+
+    history.push("/teachers")
   }
 
-export default UpdateFaculty
+  return (
+    <Base
+      title={"Welcome to Faculty Edit Section"}
+      about={"To Fill these and Update Your data"}
+    >
+      <form onSubmit={handleSubmit}>
+        <div className='input-list'>
+          <TextField
+            id="filled-basic"
+            label="Name"
+            variant="filled"
+            name='teacherName'
+            type='text'
+            onChange={handleChange}
+            value={values.teacherName}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.teacherName : ""}</div>
+
+          <TextField
+            id="filled-basic"
+            label="Subject"
+            variant="filled"
+            name='subject'
+            type='text'
+            onChange={handleChange}
+            value={values.subject}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.subject : ""}</div>
+
+          <TextField
+            id="filled-basic"
+            label="Gender"
+            variant="filled"
+            name='teacherGender'
+            type='text'
+            onChange={handleChange}
+            value={values.teacherGender}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.teacherGender : ""}</div>
+
+          <TextField
+            id="filled-basic"
+            label="Role"
+            variant="filled"
+            name='Role'
+            type='text'
+            onChange={handleChange}
+            value={values.Role}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.Role : ""}</div>
+        </div>
+        <br />
+        <Button variant="contained" type='submit'>Update Faculty</Button>
+      </form>
+    </Base>
+  )
+}
+
+export default UpdateFaculty;
--- src/Base/Components/Students/Updatestudents.js
+++ src/Base/Components/Students/Updatestudents.js
@@ -1,85 +1,125 @@
 import { Button } from '@mui/material'
 import { TextField } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { useHistory, useParams } from 'react-router-dom'
 import Base from '../../Base'
 import * as yup from 'yup'
 import { useFormik } from 'formik'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
 
 const filledValidationSchema = yup.object({
-  name:yup.string().required("Kindly update the name"),
-  batch:yup.string().required("Kindly valid the batch").min("5","Fill valid batch"),
-  gender:yup.string().required("Kindly update gender"),
-  qualification:yup.string().required("kindly update qualification")
+  name: yup.string().required("Kindly update the name"),
+  batch: yup.string().required("Kindly valid the batch").min(5, "Fill valid batch"),
+  gender: yup.string().required("Kindly update gender"),
+  qualification: yup.string().required("kindly update qualification")
 })
-function Updatestudents({student,setStudent,editIdx,setEditIdx}) {
-  const {id} =useParams()
-  const { handleSubmit,values,handleChange,errors } = useFormik({
-    initialValues:{
-      name: "",batch:"",gender:"",qualification:""
+
+function Updatestudents({ student, setStudent, editIdx, setEditIdx }) {
+  const { id } = useParams()
+  const history = useHistory()
+
+  const { handleSubmit, values, handleChange, errors } = useFormik({
+    initialValues: {
+      name: "",
+      batch: "",
+      gender: "",
+      qualification: ""
     },
-    validationSchema :filledValidationSchema,
-    onSubmit:(x)=>{
-      console.log("Onsubmit",x)
-      needupdate(x)
+    validationSchema: filledValidationSchema,
+    onSubmit: (x) => {
+      console.log("Onsubmit", x)
+      needUpdate(x)
     }
   })
 
-  const history =useHistory()
   const editStudent = student[editIdx]
-  const [name,setName]=useState("")
-  const [batch,setBatch]=useState("")
-  const [gender,setGender]=useState("")
-  const [qualification,setQualification]=useState("")
-  
-  useEffect(()=>{
-       setName(editStudent.name)
-       setBatch(editStudent.batch)
-       setGender(editStudent.gender)
-       setQualification(editStudent.qualification)
-       console.log("edit",editStudent)
-  },[editStudent])
 
-  async function needupdate() {
-    const updateobj = { name:values.name, batch:values.batch, qualification:values.qualification, gender:values.gender }
-     const response =await fetch(`https://644f880bba9f39c6ab65caa9.mockapi.io/users/${editStudent.id}`,{
-      method:"PUT",
-      body:JSON.stringify(updateobj),
-      headers:{
-        "Content-type":"application/json"
+  useEffect(() => {
+    if (editStudent) {
+      handleChange({ target: { name: 'name', value: editStudent.name } })
+      handleChange({ target: { name: 'batch', value: editStudent.batch } })
+      handleChange({ target: { name: 'gender', value: editStudent.gender } })
+      handleChange({ target: { name: 'qualification', value: editStudent.qualification } })
+    }
+  }, [editStudent, handleChange])
+
+  async function needUpdate(updateData) {
+    const updateObj = {
+      name: updateData.name,
+      batch: updateData.batch,
+      qualification: updateData.qualification,
+      gender: updateData.gender
+    }
+
+    const response = await fetch(`https://644f880bba9f39c6ab65caa9.mockapi.io/users/${editStudent.id}`, {
+      method: "PUT",
+      body: JSON.stringify(updateObj),
+      headers: {
+        "Content-type": "application/json"
       }
-     })
-     const data= await response.json()
-     if(data){
-      console.log(updateobj)
-      student[id] = updateobj
+    })
+
+    const data = await response.json()
+    if (data) {
+      console.log(updateObj)
+      student[id] = updateObj
       setStudent([...student])
-     }
+    }
     history.push("/students")
   }
-  
+
   return (
     <Base
-    title={"Welcome to Student Edit Section "}
-    about ={"To Fill these and Update Your data"}
+      title={"Welcome to Student Edit Section "}
+      about={"To Fill these and Update Your data"}
     >
       <form onSubmit={handleSubmit}>
-   <div className='input-list'>
-      <TextField id="filled-basic" label="Name" variant="filled"  name='name' type='name' 
-      onChange={handleChange} value={values.name} />
-      <div style={{color:"crimson"}}>{errors ? errors.name : ""}</div>
-      <TextField id="filled-basic" label="Batch" variant="filled" name='batch' type='batch'
-      onChange={handleChange} value={values.batch}/>
-      <div style={{color:"crimson"}}>{errors ? errors.batch : ""}</div>
-      <TextField id="filled-basic" label="Gender" variant="filled"  name='gender' type='gender'
-      onChange={handleChange} value={values.gender}/>
-      <div style={{color:"crimson"}}>{errors ? errors.gender : ""}</div>
-      <TextField id="filled-basic" label="Qualification" variant="filled"  name='qualification' type='qualification'
-      onChange={handleChange} value={values.qualification}/>
-      <div style={{color:"crimson"}}>{errors ? errors.qualification : ""}</div>
-      </div><br/>
-      <Button variant="contained" type='submit'>Update Students</Button>
+        <div className='input-list'>
+          <TextField
+            id="filled-basic"
+            label="Name"
+            variant="filled"
+            name='name'
+            type='name'
+            onChange={handleChange}
+            value={values.name}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.name : ""}</div>
+
+          <TextField
+            id="filled-basic"
+            label="Batch"
+            variant="filled"
+            name='batch'
+            type='batch'
+            onChange={handleChange}
+            value={values.batch}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.batch : ""}</div>
+
+          <TextField
+            id="filled-basic"
+            label="Gender"
+            variant="filled"
+            name='gender'
+            type='gender'
+            onChange={handleChange}
+            value={values.gender}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.gender : ""}</div>
+
+          <TextField
+            id="filled-basic"
+            label="Qualification"
+            variant="filled"
+            name='qualification'
+            type='qualification'
+            onChange={handleChange}
+            value={values.qualification}
+          />
+          <div style={{ color: "crimson" }}>{errors ? errors.qualification : ""}</div>
+        </div>
+        <br />
+        <Button variant="contained" type='submit'>Update Students</Button>
       </form>
     </Base>
   )
